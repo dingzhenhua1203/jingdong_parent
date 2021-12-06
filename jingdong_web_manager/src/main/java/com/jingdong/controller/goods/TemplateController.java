7863e0ef7e2dee8ee2cd7127664cc4b48b92a3b9
dingzhenhua1203 <1194319307@qq.com>
2021-12-06 23:57:49 +0800

    修改代码

---
+++ jingdong_web_manager/src/main/java/com/jingdong/controller/goods/TemplateController.java
@@ -0,0 +1,64 @@
+package com.jingdong.controller.goods;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.jingdong.model.base.PageResult;
+import com.jingdong.model.base.ResultMsg;
+import com.jingdong.pojo.goods.Template;
+import com.jingdong.service.goods.TemplateService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+
+@RestController
+@RequestMapping("/template")
+@CrossOrigin
+public class TemplateController {
+
+    @Reference
+    private TemplateService templateService;
+
+    @GetMapping("/findAll")
+    public List<Template> findAll(){
+        return templateService.findAll();
+    }
+
+    @GetMapping("/findPage")
+    public PageResult<Template> findPage(int page, int size){
+        return templateService.findPage(page, size);
+    }
+
+    @PostMapping("/findList")
+    public List<Template> findList(@RequestBody Map<String,Object> searchMap){
+        return templateService.findList(searchMap);
+    }
+
+    @PostMapping("/findPage")
+    public PageResult<Template> findPage(@RequestBody Map<String,Object> searchMap,int page, int size){
+        return  templateService.findPage(searchMap,page,size);
+    }
+
+    @GetMapping("/findById")
+    public Template findById(Integer id){
+        return templateService.findById(id);
+    }
+
+
+    @PostMapping("/add")
+    public ResultMsg add(@RequestBody Template template){
+        templateService.add(template);
+        return new ResultMsg();
+    }
+
+    @PostMapping("/update")
+    public ResultMsg update(@RequestBody Template template){
+        templateService.update(template);
+        return new ResultMsg();
+    }
+
+    @GetMapping("/delete")
+    public ResultMsg delete(Integer id){
+        templateService.delete(id);
+        return new ResultMsg();
+    }
+
+}
