0d14eb6b58110e08615de75c81d299ad36928af4
dingzhenhua1203 <1194319307@qq.com>
2021-12-29 00:05:01 +0800

    thymeleaf开发index

--- jingdong_web_protal/src/main/java/com/jingdong/controller/IndexController.java
+++ jingdong_web_protal/src/main/java/com/jingdong/controller/IndexController.java
@@ -1,14 +1,18 @@
 package com.jingdong.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
+import com.jingdong.goods.service.CategoryService;
 import com.jingdong.pojo.business.Ad;
 import com.jingdong.service.business.AdService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Map;
 
 @Controller
 public class IndexController {
@@ -16,10 +20,24 @@ public class IndexController {
     @Reference
     private AdService adService;
 
+    @Reference
+    private CategoryService categoryService;
+
     @GetMapping("index")
     public String index(Model model) {
         List<Ad> data = adService.findByPosition("web_index_lb");
-        model.addAttribute("lunbotu",data);
-        return  "index";
+        model.addAttribute("lunbotu", data);
+
+        List<Map> categoryTree = categoryService.findCategoryTree();
+        model.addAttribute("categoryTree", categoryTree);
+        return "index";
+    }
+
+    @ResponseBody
+    @GetMapping("test")
+    public List<Map> testData(Model model) {
+        List<Map> categoryTree = categoryService.findCategoryTree();
+
+        return categoryTree;
     }
 }
