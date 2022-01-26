e29bcbc2f6a4af566e654589ca07867ce5432337
eric.ding <eric.ding@movit-tech.com>
2022-01-26 18:08:05 +0800

    es

--- jingdong_common_service/src/main/java/com/jingdong/util/es/ESHighLevelRestUtil.java
+++ jingdong_common_service/src/main/java/com/jingdong/util/es/ESHighLevelRestUtil.java
@@ -1,5 +1,6 @@
 package com.jingdong.util.es;
 
+import com.alibaba.fastjson.JSON;
 import org.apache.http.HttpHost;
 import org.elasticsearch.ElasticsearchException;
 import org.elasticsearch.action.admin.indices.create.CreateIndexRequest;
@@ -10,15 +11,30 @@ import org.elasticsearch.action.delete.DeleteRequest;
 import org.elasticsearch.action.delete.DeleteResponse;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.action.support.replication.ReplicationResponse;
 import org.elasticsearch.action.DocWriteResponse;
+import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.client.core.CountRequest;
+import org.elasticsearch.client.core.CountResponse;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.index.query.BoolQueryBuilder;
+import org.elasticsearch.index.query.MatchQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.index.query.TermQueryBuilder;
 import org.elasticsearch.rest.RestStatus;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHits;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightField;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -27,205 +43,172 @@ public class ESHighLevelRestUtil {
     @Autowired
     private RestHighLevelClient client;
 
+    public String indexCreate(String index, String indexType,
+                              Map<String, Object> properties) throws Exception {
+
+        /**
+         * 一般我们初学时会把这些与数据库进行对照方便理解
+         *
+         * Index->Database
+         * Type->Table （最新版本已经不使用Type了，所以很多人会奇怪为什么去掉了？ES并非和数据库是相同的，所以不要完全按数据库的方式来看ES）
+         * Document->Row
+         */
+        IndexRequest indexRequest = new IndexRequest(index).id("");
+        indexRequest.source(JSON.toJSONString(properties), XContentType.JSON);
+        indexRequest.timeout("1s");
+        IndexResponse indexResponse = client.index(indexRequest,
+                RequestOptions.DEFAULT);
+        System.out.println(indexResponse.getResult().toString());
+        return indexResponse.getResult().toString();
+    }
+
     /**
-     * 验证索引是否存在
+     * 查询所有matchAllQuery
      *
-     * @param index 索引名称
-     * @return
-     * @throws Exception
-     *//*
-    public boolean indexExists(String index) throws Exception {
-        GetIndexRequest request = new GetIndexRequest();
-        request.indices(index);
-        request.local(false);
-        request.humanReadable(true);
+     * @throws IOException
+     */
+    void search(String index) throws IOException {
+        SearchRequest searchRequest = new SearchRequest(index);
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+        searchSourceBuilder.query(QueryBuilders.matchAllQuery());
+        searchRequest.source(searchSourceBuilder);
 
-        boolean exists = client.indices().exists(request);
-        return exists;
+        SearchResponse searchResponse = client.search(searchRequest, RequestOptions.DEFAULT);
+        searchResponse.getHits().forEach(res -> {
+            // log.info(res.toString());
+        });
+
     }
 
-    *//**
-     * @param index
-     * @param indexType
-     * @param properties 结构: {name:{type:text}} {age:{type:integer}}
-     * @return
-     * @throws Exception
-     *//*
-    public boolean indexCreate(String index, String indexType,
-                               Map<String, Object> properties) throws Exception {
-
-        if (indexExists(index)) {
-            return true;
-        }
-        CreateIndexRequest request = new CreateIndexRequest(index);
-        request.settings(Settings.builder().put("index.number_of_shards", 3)
-                .put("index.number_of_replicas", 2));
-
-        Map<String, Object> jsonMap = new HashMap<>();
-        Map<String, Object> mapping = new HashMap<>();
-        mapping.put("properties", properties);
-        jsonMap.put(indexType, mapping);
-        request.mapping(indexType, jsonMap);
-
-        CreateIndexResponse createIndexResponse = client.indices().create(request);
-        boolean acknowledged = createIndexResponse.isAcknowledged();
-        return acknowledged;
-    }
-
-
-    *//**
-     * 创建更新文档
+    /**
+     * 精确查询termQuery
      *
-     * @param index
-     * @param indexType
-     * @param documentId
-     * @param josonStr
-     * @return
-     * @throws Exception
-     *//*
-    public boolean documentCreate(String index, String indexType,
-                                  String documentId, String josonStr) throws Exception {
-        IndexRequest request = new IndexRequest(index, indexType, documentId);
+     * @throws IOException
+     */
+    void termQuery(String index, String key, String value) throws IOException {
+        SearchRequest searchRequest = new SearchRequest(index);
+        SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
 
-        request.source(josonStr, XContentType.JSON);
-        IndexResponse indexResponse = client.index(request);
+        // termQuery有两种写法
+        // sourceBuilder.query(QueryBuilders.termQuery(key, value));
+        TermQueryBuilder termQuery = new TermQueryBuilder(key, value);
+        sourceBuilder.query(termQuery);
+        searchRequest.source(sourceBuilder);
 
-        if (indexResponse.getResult() == DocWriteResponse.Result.CREATED
-                || indexResponse.getResult() == DocWriteResponse.Result.UPDATED) {
-            return true;
-        }
-        ReplicationResponse.ShardInfo shardInfo = indexResponse.getShardInfo();
-        if (shardInfo.getTotal() != shardInfo.getSuccessful()) {
-            return true;
-        }
-        if (shardInfo.getFailed() > 0) {
-            for (ReplicationResponse.ShardInfo.Failure failure : shardInfo
-                    .getFailures()) {
-                throw new Exception(failure.reason());
-            }
-        }
-        return false;
+        SearchResponse searchResponse = client.search(searchRequest, RequestOptions.DEFAULT);
+        searchResponse.getHits().forEach(res -> {
+            // log.info(res.toString());
+        });
     }
 
-    *//**
-     * 创建更新索引
+    /**
+     * 模糊查询matchQuery
      *
-     * @param index
-     * @param indexType
-     * @param documentId
-     * @param map
-     * @return
-     * @throws Exception
-     *//*
-    public boolean documentCreate(String index, String indexType,
-                                  String documentId, Map<String, Object> map) throws Exception {
-        IndexRequest request = new IndexRequest(index, indexType, documentId);
-
-        request.source(map);
-        IndexResponse indexResponse = client.index(request);
-
-        if (indexResponse.getResult() == DocWriteResponse.Result.CREATED
-                || indexResponse.getResult() == DocWriteResponse.Result.UPDATED) {
-            return true;
-        }
-        ReplicationResponse.ShardInfo shardInfo = indexResponse.getShardInfo();
-        if (shardInfo.getTotal() != shardInfo.getSuccessful()) {
-            return true;
-        }
-        if (shardInfo.getFailed() > 0) {
-            for (ReplicationResponse.ShardInfo.Failure failure : shardInfo
-                    .getFailures()) {
-                throw new Exception(failure.reason());
-            }
-        }
-        return false;
+     * @throws IOException
+     */
+    void matchQuery(String index, String key, String value) throws IOException {
+        SearchRequest searchRequest = new SearchRequest(index);
+        SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
+        sourceBuilder.query(QueryBuilders.matchQuery(key, value));
+        searchRequest.source(sourceBuilder);
+        SearchResponse searchResponse = client.search(searchRequest, RequestOptions.DEFAULT);
+        searchResponse.getHits().forEach(res -> {
+            // log.info(res.toString());
+        });
     }
 
-    *//**
-     * 创建索引
+    /**
+     * 模糊查询boolQuery
      *
-     * @param index
-     * @param indexType
-     * @param josonStr
-     * @return
-     * @throws Exception
-     *//*
-    public String documentCreate(String index, String indexType, String josonStr)
-            throws Exception {
-        IndexRequest request = new IndexRequest(index, indexType);
-
-        request.source(josonStr, XContentType.JSON);
-        IndexResponse indexResponse = client.index(request);
-
-        String id = indexResponse.getId();
-        if (indexResponse.getResult() == DocWriteResponse.Result.CREATED
-                || indexResponse.getResult() == DocWriteResponse.Result.UPDATED) {
-            return id;
-        }
-        ReplicationResponse.ShardInfo shardInfo = indexResponse.getShardInfo();
-        if (shardInfo.getTotal() != shardInfo.getSuccessful()) {
-            return id;
-        }
-        if (shardInfo.getFailed() > 0) {
-            for (ReplicationResponse.ShardInfo.Failure failure : shardInfo
-                    .getFailures()) {
-                throw new Exception(failure.reason());
-            }
-        }
-        return null;
+     * @throws IOException
+     */
+    void boolQuery(String index, String key, String value) throws IOException {
+        SearchRequest searchRequest = new SearchRequest(index);
+        SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
+        BoolQueryBuilder boolQueryBuilder = QueryBuilders.boolQuery();
+        boolQueryBuilder.must(QueryBuilders.matchQuery("scompCode", "G0000001"));
+        // 模糊查询
+        boolQueryBuilder.filter(QueryBuilders.wildcardQuery("itemDesc", "*手机*"));
+        // 范围查询 from:相当于闭区间; gt:相当于开区间(>) gte:相当于闭区间 (>=) lt:开区间(<) lte:闭区间 (<=)
+        boolQueryBuilder.filter(QueryBuilders.rangeQuery("itemPrice").from(4500).to(8899));
+        // boolQueryBuilder.filter(QueryBuilders.termQuery("mobile", ""));
+        // boolQueryBuilder.filter(QueryBuilders.rangeQuery("num").gte(1));
+        sourceBuilder.query(boolQueryBuilder);
+        searchRequest.source(sourceBuilder);
+        SearchResponse searchResponse = client.search(searchRequest, RequestOptions.DEFAULT);
+        searchResponse.getHits().forEach(res -> {
+            // log.info(res.toString());
+        });
     }
 
-    *//**
-     * 创建索引
+    void countQuery(String index, String key, String value) {
+        BoolQueryBuilder boolBuilder = QueryBuilders.boolQuery();
+        SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
+        boolBuilder.filter(QueryBuilders.rangeQuery("num").gte(1));
+        sourceBuilder.query(boolBuilder);
+
+        CountRequest countRequest = new CountRequest(index);
+        countRequest.query(boolBuilder);
+        CountResponse count = null;
+        try {
+            count = client.count(countRequest, RequestOptions.DEFAULT);
+        } catch (IOException e) {
+            // logger.error("[ElasticSearchService] search error!", e);
+        }
+    }
+
+    /**
+     * 高亮查询
      *
-     * @param index
-     * @param indexType
-     * @param map
-     * @return
-     * @throws Exception
-     *//*
-    public String documentCreate(String index, String indexType,
-                                 Map<String, Object> map) throws Exception {
-        IndexRequest request = new IndexRequest(index, indexType);
+     * @throws IOException
+     */
+    void highlight() throws IOException {
+        SearchRequest searchRequest = new SearchRequest("high-index01");
 
-        request.source(map);
-        IndexResponse indexResponse = client.index(request);
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+        searchSourceBuilder.highlighter(highlightBuilder());
+        searchSourceBuilder.query(new MatchQueryBuilder("title", "你好"));
 
-        String id = indexResponse.getId();
-        if (indexResponse.getResult() == DocWriteResponse.Result.CREATED
-                || indexResponse.getResult() == DocWriteResponse.Result.UPDATED) {
-            return id;
-        }
-        ReplicationResponse.ShardInfo shardInfo = indexResponse.getShardInfo();
-        if (shardInfo.getTotal() != shardInfo.getSuccessful()) {
-            return id;
-        }
-        if (shardInfo.getFailed() > 0) {
-            for (ReplicationResponse.ShardInfo.Failure failure : shardInfo
-                    .getFailures()) {
-                throw new Exception(failure.reason());
-            }
-        }
-        return null;
+        searchRequest.source(searchSourceBuilder);
+        SearchResponse searchResponse = client.search(searchRequest, RequestOptions.DEFAULT);
+        printHits(searchResponse);
     }
 
-    public boolean documentDelete(String index, String indexType,
-                                  String documentId) throws Exception {
-        DeleteRequest request = new DeleteRequest(index, indexType, documentId);
-        DeleteResponse deleteResponse = client.delete(request);
-        if (deleteResponse.getResult() == DocWriteResponse.Result.NOT_FOUND) {
-            return true;
+
+    /**
+     * 词频统计aggregate
+     */
+    public void aggregate() throws IOException {
+
+    }
+
+
+    /**
+     * 词频统计term vectors
+     */
+    public void term_vectors() throws IOException {
+
+    }
+
+    /* 私有方法*/
+    private HighlightBuilder highlightBuilder() {
+        HighlightBuilder highlightBuilder = new HighlightBuilder();
+        HighlightBuilder.Field highlightTitle =
+                new HighlightBuilder.Field("title");
+        highlightBuilder.field(highlightTitle);
+        highlightBuilder.preTags("<font size=\"3\" color=\"red\">");
+        highlightBuilder.postTags("</font>");
+        return highlightBuilder;
+    }
+
+    private void printHits(SearchResponse searchResponse) {
+        SearchHits hits = searchResponse.getHits();
+        for (SearchHit hit : hits.getHits()) {
+            Map<String, HighlightField> highlightFields = hit.getHighlightFields();
+            HighlightField highlight = highlightFields.get("title");
+            //Text[] fragments = highlight.fragments();
+            //String fragmentString = fragments[0].string();
+            //log.info(fragmentString);
         }
-        ReplicationResponse.ShardInfo shardInfo = deleteResponse.getShardInfo();
-        if (shardInfo.getTotal() != shardInfo.getSuccessful()) {
-            return true;
-        }
-        if (shardInfo.getFailed() > 0) {
-            for (ReplicationResponse.ShardInfo.Failure failure : shardInfo
-                    .getFailures()) {
-                throw new Exception(failure.reason());
-            }
-        }
-        return false;
-    }*/
+    }
 }
--- jingdong_web_protal/src/main/java/com/jingdong/controller/SearchController.java
+++ jingdong_web_protal/src/main/java/com/jingdong/controller/SearchController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 import java.util.Map;
@@ -14,6 +15,15 @@ public class SearchController {
 
     @GetMapping("search")
     public String index(Model model) {
+
+        return "search";
+    }
+
+
+
+    @ResponseBody
+    @GetMapping("pushes")
+    public String PushES() {
 
         return "search";
     }
