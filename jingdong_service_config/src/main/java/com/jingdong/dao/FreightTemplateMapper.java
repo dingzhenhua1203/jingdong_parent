82462f9935051cb3f7d57679542fd3acd75c94cb
dingzhenhua1203 <1194319307@qq.com>
2022-02-20 20:47:19 +0800

    移除多余模块

--- jingdong_service_config/src/main/java/com/jingdong/dao/FreightTemplateMapper.java
+++
@@ -1,8 +0,0 @@
-package com.jingdong.dao;
-
-import com.jingdong.pojo.config.FreightTemplate;
-import tk.mybatis.mapper.common.Mapper;
-
-public interface FreightTemplateMapper extends Mapper<FreightTemplate> {
-
-}
